9c09bb1650f20b5caa0c7745242be8d8e67c87b5
isotherm <kirk.meyer@alpaxo.com>
2016-07-12 09:20:58 -0500

    Add platforms and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author=u'Kirk Meyer',
     author_email='kirk.meyer@alpaxo.com',
     license='MIT',
+    platforms='any',
     py_modules=['lektor_gae'],
     entry_points={
         'lektor.plugins': [
@@ -17,5 +18,15 @@ setup(
     install_requires=[
         'Lektor',
         'PyYAML',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
